63054dcd5dee5728d52e5f2690a7c4717206f390
Diogo Montalvão <109600744+DiogoMontalvao@users.noreply.github.com>
2025-01-16 16:58:38 -0300

    1.6 - Conditional statements and conditional operation

--- part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
+++ part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
@@ -4,7 +4,24 @@ import java.util.Scanner;
 public class LargerThanOrEqualTo {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+    
+        System.out.println("Give the second number:");
+        int second = Integer.valueOf(scanner.nextLine());
+        
+        if (first > second) {
+            System.out.println(String.format("Greater number is: %s", first));
+            return;
+        }
+        
+        if (first == second) {
+            System.out.println("The numbers are equal!");
+            return;
+        }
+        
+        System.out.println(String.format("Greater number is: %s", second));
     }
 }
--- part01-Part01_36.LeapYear/src/main/java/LeapYear.java
+++ part01-Part01_36.LeapYear/src/main/java/LeapYear.java
@@ -5,6 +5,20 @@ public class LeapYear {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        
+        System.out.println("Give a year:");
+        int year = scan.nextInt();
+        
+        if (year % 100 == 0 && year % 400 != 0) {
+            System.out.println("The year is not a leap year.");
+            return;
+        }
+        
+        if (year % 4 != 0) {
+            System.out.println("The year is not a leap year.");
+            return;
+        }
+        
+        System.out.println("The year is a leap year.");
     }
 }
